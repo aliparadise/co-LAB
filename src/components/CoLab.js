b43dfc1f7448e09ab7bf2e70d3cf0b6d9d632f07
Ali Paradise <alison.paradise@gmail.com>
2022-01-28 13:37:59 -0600

    added colab component

---
+++ src/components/CoLab.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const CoLab = () => {
+
+    return (
+        <h1>co-LAB</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { CoLab } from "./components/CoLab";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoLab />
   </React.StrictMode>,
   document.getElementById('root')
 );
