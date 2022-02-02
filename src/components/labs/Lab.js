6fcbbff04a8031e3411a78ad04e74a491f533373
Ali Paradise <alison.paradise@gmail.com>
2022-02-02 10:12:59 -0600

    added facilitator select to individual lab

--- src/components/labs/Lab.js
+++ src/components/labs/Lab.js
@@ -3,17 +3,28 @@ import { useParams } from "react-router-dom"
 
 export const Lab = () => {
     const [ lab, singleLab ] = useState({})
+    const [ facilitator, selectFacilitator] = useState([])
 
     const { labId } = useParams()
 
     useEffect (
         () => {
             return fetch(`http://localhost:8088/labs/${labId}?_expand=facilitator`)
-            .then(response => response.json())
-            .then((data) => {
-                singleLab(data)
+                .then(response => response.json())
+                .then((data) => {
+                    singleLab(data)
             })
         },[ labId ]
+    )
+
+    useEffect (
+        () => {
+            return fetch("http://localhost:8088/facilitators")
+                .then(response => response.json())
+                .then((data) => {
+                    selectFacilitator(data)
+                })
+        }, []
     )
 
     return (
@@ -25,7 +36,20 @@ export const Lab = () => {
                 <div className="lab__address">{ lab.address }</div>
                 <div className="lab__date">{ lab.date }</div>
                 <div className="lab__time">{ lab.time }</div>
-                <div className="lab__facilitator">Facilitator will be: { lab.facilitator?.name }</div>
+                <div className="lab__facilitator">
+                    Facilitator will be: { lab.facilitator?.name }</div>
+                    <select id="facilitator">
+                        { 
+                            facilitator.map(
+                                (facilitator) => {
+                                    return <option key={`facilitator--${facilitator.id}`}>
+                                        { facilitator.name }
+                                    </option>
+                                }
+
+                            )
+                        }
+                    </select>
             </section>
         </>
     )
